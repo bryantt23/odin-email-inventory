b55c98eed228628a1073d0e69f6b92a64fb9e54e
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-11-26 01:37:02 -0800

    make text area

--- models/message.js
+++ models/message.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
   category: { type: String, required: true },
   title: { type: String, required: true, minLength: 2 },
-  text: { type: String, required: true, minLength: 7 }
+  text: { type: String, required: true, minLength: 5 }
 });
 
 // Export model
