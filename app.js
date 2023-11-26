c6ce89156e670716f1c586d8c5afcac20e53e961
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-11-25 17:16:14 -0800

    get messages on page

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const messagesRouter = require('./routes/messages');
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.MONGODB_URI;
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 const app = express();
 
