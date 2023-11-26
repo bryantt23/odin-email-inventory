a1d4f421c0db0d01f2a3b91a94781aa83628d261
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-11-26 02:05:29 -0800

    add login page

--- app.js
+++ app.js
@@ -19,17 +19,38 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
+const session = require('express-session');
+
 const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+app.post('/login', (req, res) => {
+  const submittedPassword = req.body.password;
+
+  if (submittedPassword === process.env.PASSWORD) {
+    req.session.isAuthenticated = true;
+    res.redirect('/messages');
+  } else {
+    res.send('Incorrect Password');
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
